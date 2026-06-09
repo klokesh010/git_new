89b6fe13d70715996283ab5671c6e67d78a8796b
lokesh <klokeshprasad010@gmail.com>
2026-06-09 12:07:59 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn lokesh
+          Learn lokesh prasad
         </a>
       </header>
     </div>
